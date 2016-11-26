902d38b742a116045a0ffc03e513fdead05bf344
Layton Whiteley <layton.whiteley@gmail.com>
2016-11-25 22:47:58 -0400

    add zip packaging

---
+++ src/wp-url.js
@@ -0,0 +1,3 @@
+const WP_BASE_URL = process.env.WP_BASE_URL || 'http://localhost:8888/wordpress';
+export const WP_URL = `${WP_BASE_URL}/wp-json/wp/v2`;
+export const WP_MENU_URL = `${WP_BASE_URL}/wp-json/wp-api-menus/v2`;
