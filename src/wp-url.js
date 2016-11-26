f5af91093d7e5bd205139f66dc620b2aa985f0a7
Layton Whiteley <layton.whiteley@gmail.com>
2016-11-26 01:01:23 -0400

    reuse root const

--- src/wp-url.js
+++ src/wp-url.js
@@ -1,4 +1,4 @@
-const WP_BASE_URL = 'http://localhost:8888/wordpress';
+export const ROOT = '/wordpress'
+const WP_BASE_URL = `http://localhost:8888${ROOT}`;
 export const WP_URL = `${WP_BASE_URL}/wp-json/wp/v2`;
 export const WP_MENU_URL = `${WP_BASE_URL}/wp-json/wp-api-menus/v2`;
-export const ROOT = '/wordpress'
